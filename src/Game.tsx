a016b3c8700344dc5ffffea6aa6d40d101a15b61
Hua <hua.void@gmail.com>
2022-12-06 00:07:39 -0500

    AI & Docker update

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,7 +1,14 @@
-import React from "react";
-import { GameProps, GameState} from "./types";
+import React, { MouseEvent, MouseEventHandler } from "react";
+import { GameProps, GameState, Piece} from "./types";
 import BoardElement from "./Board";
 import "./App.css";
+import axios from "axios";
+
+
+type NextButtonProps = {
+  handleClick: MouseEventHandler;
+  show: boolean;
+};
 
 export default class GameElement extends React.Component<GameProps, GameState> {
   constructor(props: GameProps) {
@@ -13,7 +20,29 @@ export default class GameElement extends React.Component<GameProps, GameState> {
     };
   }
  
+  handleClick: MouseEventHandler = (event: MouseEvent) => {
+    event.preventDefault();
+    
+    const pieces = this.props.game?.boards.at(-1)?.pieces;
+    let piece;
+    for (let i = 0; i < 90; i++) {
+      if (pieces?.hasOwnProperty(i.toString())) {
+        piece = pieces[i.toString()];
+        break;
+      }
+    }
+    console.log(piece);
 
+    const url = piece?._links?.self.href as string;
+    axios
+        .put(url + `?mode1=${this.props.mode1}&mode2=${this.props.mode2}`, piece)
+        .then((response) => {
+          console.log("succeeded");
+        }).catch((error) => {
+          console.log("error" + error);
+        });
+    
+  }
   render(): React.ReactNode {
     // console.log(this.props.game);
     // console.log(this.props.user);
@@ -30,8 +59,18 @@ export default class GameElement extends React.Component<GameProps, GameState> {
         <BoardElement
           board={board === undefined ? null : board}
           user={user}
+          mode1={this.props.mode1}
+          mode2={this.props.mode2}
         />
+        <br></br>
+        <NextButton show={!((this.props.mode1 === -1 && this.props.mode2 === -1) || this.props.mode1 === 0)} handleClick={this.handleClick}/>
       </div>
     );
   }
+}
+
+
+function NextButton(props: NextButtonProps): JSX.Element {
+  let style = props.show? { display: "block" } : { display: "none" };
+  return <button className="NextButton" style={style} onClick={props.handleClick}>Next</button>
 }
